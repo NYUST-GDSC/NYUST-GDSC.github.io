65bb835360a5851fd56a7ea2815696363c6eb456
fan9704 <cxz123499@gmail.com>
2023-08-17 17:06:39 +0800

    Complete Basic Navigator and Footer

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,68 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="row row-grid align-items-center mb-5">
+        <div class="col-lg-6">
+          <h3 class="font-weight-light mb-2" style="color: #4285F4;"><br>{{ $t('footer.title') }}</h3>
+          <p>{{ $t('footer.description') }}<a
+              href="https://gdsc.community.dev/national-yunlin-university-of-science-and-technology/" target="_blank"
+              style="color: #4285F4;">{{ $t('footer.shortName') }}</a>。
+          </p>
+        </div>
+        <div class="col-lg-6 text-lg-left btn-wrapper" id="social_media_list">
+          <h3 style="color: #4285F4;">{{ $t('footer.contact') }}</h3>
+          <button v-for="media in mediaList" :key="media" class="btn ">
+            <a :href="media.link" style="text-decoration: none">
+              <i :class="`mdi ${media.icon}`" ></i> {{media.name}}
+            </a>
+          </button>
+        </div>
+      </div>
+      <div class="row align-items-center justify-content-md-between">
+        <div class="col-md-6">
+          <div class="copyright" style="margin-bottom: 2rem; border-bottom: 2rem">
+            &copy;
+            {{ new Date().getFullYear() }}
+            <router-link to="/" style="color: #4285F4;">{{ $t('footer.shortName') }}</router-link>•
+            <router-link to="/code_of_conduct/" style="color: #4285F4;">{{ $t('footer.coc') }}</router-link>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data(){
+    return{
+      mediaList:[
+        {
+          "name":"Instagram",
+          "link":"https://www.instagram.com/gdsc_nyust/",
+          "icon":"mdi-instagram"
+        },
+        {
+          "name":"Discord",
+          "link":"https://discord.gg/2k7SbzeByJ",
+          "icon":"mdi-discord"
+        },
+        {
+          'name':"bevy",
+          "link":"https://gdsc.community.dev/national-yunlin-university-of-science-and-technology/",
+          "icon":"mdi-search-web"
+        }
+      ]
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.footer{
+  background-color: #F4F5F7;
+}
+</style>
---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="logo navbarsection">
+    <img :src="'/gdsc-nyust.png'" id="logo-size" alt />
+    <router-link to="/" class="nav-link active btn navbarbtnred" aria-current="page">{{$t("nav.home")}}</router-link>
+    <router-link to="/events" class="nav-link active btn navbarbtnblue" aria-current="page">{{$t("nav.events")}}</router-link>
+    <router-link to="/annual" class="nav-link active btn navbarbtngreen" aria-current="page">{{$t("nav.annual")}}</router-link>
+    <router-link to="/other-campus" class="nav-link active btn navbarbtnyellow" aria-current="page">{{$t("nav.other")}}</router-link>
+    <select v-model="selected" @change="setLocale">
+      <option v-for="option in options" v-bind:value="option.value"> {{ option.text }}</option>
+    </select>
+  </div>
+<!--  <nav class="navbar bg-body-tertiary">-->
+<!--    <div class="container-fluid">-->
+<!--      <a class="navbar-brand" href="#">-->
+<!--        <img src="../../assets/gdsc-nyust.png" alt="Logo" height="24" class="d-inline-block align-text-top">-->
+<!--      </a>-->
+<!--      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--        <span class="navbar-toggler-icon"></span>-->
+<!--      </button>-->
+<!--      <div class="collapse navbar-collapse" id="navbarNav">-->
+<!--        <ul class="navbar-nav">-->
+<!--          <li class="nav-item">-->
+<!--            <router-link to="/" class="nav-link active" aria-current="page">Home</router-link>-->
+<!--          </li>-->
+<!--          <li class="nav-item">-->
+<!--            <router-link to="/events" class="nav-link active" aria-current="page">Events</router-link>-->
+<!--          </li>-->
+<!--          <li class="nav-item">-->
+<!--            <router-link to="/annual-events" class="nav-link active" aria-current="page">Annual Events</router-link>-->
+<!--          </li>-->
+<!--          <li class="nav-item">-->
+<!--            <router-link to="/other-campus" class="nav-link active" aria-current="page">Other Campus</router-link>-->
+<!--          </li>-->
+<!--          <li class="nav-item">-->
+<!--            <select v-model="selected" @change="setLocale">-->
+<!--              <option v-for="option in options" v-bind:value="option.value"> {{ option.text }}</option>-->
+<!--            </select>-->
+<!--          </li>-->
+<!--        </ul>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </nav>-->
+</template>
+
+<script>
+import {ref} from 'vue'
+import i18n from "../../plugins/i18n.js";
+export default {
+  name: "Nav",
+  setup() {
+    let lang = i18n.global.locale
+    let selected = ref(lang)
+    const options = ref([
+      { text: 'English', value: 'en' },
+      { text: '中文', value: 'zh' },
+    ])
+
+    const setLocale = (e) => {
+      let newVal = e.target.value
+      i18n.global.locale = newVal
+    }
+
+    return {
+      setLocale, selected, options
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* navbar */
+.navbarsection {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  gap: 2.5%;
+  background-color: transparent;
+}
+
+.navbarsection.scrolled {
+  background-color: #333;
+}
+
+.navbar-item {
+  cursor: pointer;
+  color: #ffffff;
+}
+
+.logo {
+  margin: 0 2.5% 0 2.5%;
+  cursor: pointer;
+}
+#logo-size{
+  max-width: 20vw;
+}
+.navbarbtnred {
+  color: #ea4335;
+  transition: 0.5s;
+  letter-spacing: 4px;
+  cursor: pointer;
+}
+
+.navbarbtnblue {
+  color: #4285F4;
+  transition: 0.5s;
+  letter-spacing: 4px;
+  cursor: pointer;
+}
+.navbarbtngreen {
+  color: #77B55A;
+  transition: 0.5s;
+  letter-spacing: 4px;
+  cursor: pointer;
+}
+
+
+.navbarbtnyellow {
+  color: #FBBC04;
+  transition: 0.5s;
+  letter-spacing: 4px;
+  cursor: pointer;
+}
+
+</style>
