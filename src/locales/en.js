247276ae6600a8c2c91dd46b99d1db9b8293a707
fan9704 <cxz123499@gmail.com>
2023-08-17 17:06:12 +0800

    Move Static resource to public directory

--- src/locales/en.js
+++ src/locales/en.js
@@ -13,6 +13,9 @@ export default {
         what:"What is GDSC",
         intro:"Google Developer Student Clubs (GDSC) is a student community supported by Google, aimed at connecting students with professional developers, fostering students' learning and application of Google developer technologies, and providing opportunities for students to interact with and learn from technical experts. GDSC has over 1000 communities in more than 100 countries/regions worldwide. GDSC serves as an ideal community for students to learn and share technology, and provides a platform to connect with other developers and experts in the tech industry.",
     },
+    coc:{
+        title:"National Yunlin University of Science and Technology Google Developer Student Clubs - Code of Conduct",
+    },
     footer:{
         title:"Disclaimer",
         description:"GDSC Yunlin University of Science and Technology is an independent group, and our activities and opinions are not affiliated with Google. For more information about the GDSC program, please refer to ",
