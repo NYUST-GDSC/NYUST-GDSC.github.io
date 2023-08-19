d840c63eca8a0a51c92cc836e53073a5d6fbf0d9
fan9704 <cxz123499@gmail.com>
2023-08-19 14:27:09 +0800

    feat: Complete utils for school mapping and deploy

---
+++ src/utils/locationList.js
@@ -0,0 +1,378 @@
+const locationList = [
+    {
+        "lat": 23.9910732,
+        "lng": 121.6111949,
+        "name": "國立東華大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/Chapter-Icon_CaxTeX7.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 24.9936281,
+        "lng": 121.3009798,
+        "name": "國立中央大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/ncuicon.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 25.0329694,
+        "lng": 121.5654177,
+        "name": "國立臺北教育大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 23.5203579,
+        "lng": 120.440409,
+        "name": "國立中正大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 24.1477358,
+        "lng": 120.6736482,
+        "name": "國立臺中科技大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 24.0716583,
+        "lng": 120.5624474,
+        "name": "國立彰化師範大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 25.0329694,
+        "lng": 121.5654177,
+        "name": "國立政治大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/Frame%20320.jpg",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 24.9936281,
+        "lng": 121.3009798,
+        "name": "銘傳大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 25.0329694,
+        "lng": 121.5654177,
+        "name": "國立臺灣師範大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/118906550_112365360599798_663298188729706905_n.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 24.1477358,
+        "lng": 120.6736482,
+        "name": "中山醫學大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 25.0329694,
+        "lng": 121.5654177,
+        "name": "國立臺北科技大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 24.8138287,
+        "lng": 120.9674798,
+        "name": "國立陽明交通大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/new-logo_xKQoCJK.jpg",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": "23.696219261603776",
+        "lng": "120.53404333883611",
+        "name": "國立雲林科技大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/LOGO_hmZvgMt.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 24.1477358,
+        "lng": 120.6736482,
+        "name": "朝陽科技大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/GDSC_CYUT_logo_simple-01_ElH3YEG.jpg",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 24.9936281,
+        "lng": 121.3009798,
+        "name": "長庚大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 22.98133,
+        "lng": 120.2224227,
+        "name": "國立成功大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 25.0329636,
+        "lng": 121.5654268,
+        "name": "國立臺灣大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/gdsc_kTxIhI3.jpg",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 24.9721514,
+        "lng": 121.2053963,
+        "name": "元智大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 22.6495866,
+        "lng": 120.2685799,
+        "name": "國立中山大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/B2961889-6EFC-4CAC-B3EB-085A926FDA38.PNG",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 25.0265985,
+        "lng": 121.4178347,
+        "name": "輔仁大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/iOS%20%E7%9A%84%E5%BD%B1%E5%83%8F.jpg",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 25.1719805,
+        "lng": 121.4433706,
+        "name": "淡江大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/New%20Logo.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 25.0409201,
+        "lng": 121.5720055,
+        "name": "臺北醫學大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/chapter_banners/Copy%20of%20dsc-ecosystem-profilepicture-linkedin_KYjAziA.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 23.7162184,
+        "lng": 120.4242401,
+        "name": "國立虎尾科技大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 24.1430604,
+        "lng": 120.6631289,
+        "name": "國立臺中教育大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    },
+    {
+        "lat": 24.9936281,
+        "lng": 121.3009798,
+        "name": "國立中興大學",
+        "icon": {
+            "options": {
+                "iconUrl": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,h_200,q_auto:good,w_200/v1/gcs/platform-data-goog/event_banners/gdev-eccosystems-bevy-chapters-thumbnail_x4z1EBy.png",
+                "iconSize": [
+                    50,
+                    50
+                ]
+            },
+            "_initHooksCalled": true
+        }
+    }
+]
+export default locationList
